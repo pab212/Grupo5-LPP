aa64f5fd57adb555903ec1850fb534c343107e9c
NotGormi <70240779+NotGormi@users.noreply.github.com>
2025-03-28 11:51:08 -0300

    Implementación de selección de categorías
    hola chicos bienvenidos al tutorial de c, en este tutorial se agrego la seleccion de categorias a la hora de elegir los datos que se quieren obtener y sus procesos correspondientes

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include "../include/csv_reader.h"
 #include "../include/metrics.h"
+#include <string.h>
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
@@ -24,19 +25,54 @@ int main(int argc, char *argv[]) {
     }
 
     printf("Se cargaron %d órdenes.\n", iOrders);
+    
+    // Selección de categorías para demostrar datos
+    char aux[4];
+    char *categorias; 
+    categorias = aux;
+    while (1){
+        printf("¿Que datos quiere conocer?: \n");
+        scanf("%s", categorias);
+        if (strcmp(categorias, "pms") == 0){
+            char *mas_vendida = pizza_mas_vendida(iOrders, aOrders);
+            printf("Pizza más vendida: %s\n", mas_vendida);
+            }
+        else if (strcmp(categorias, "pls") == 0){
+            char *menos_vendida = pizza_menos_vendida(iOrders, aOrders);
+            printf("Pizza menos vendida: %s\n", menos_vendida);
+        }
+        else if (strcmp(categorias, "dms") == 0){
+            dia_mas_dinero();
+        
+        }
+        else if (strcmp(categorias, "dls") == 0){
+            dia_menos_dinero();
+        }
+        else if (strcmp(categorias, "dmsp") == 0){
+            dia_mas_pizza();
+        }
+        else if (strcmp(categorias, "dlsp") == 0){
+            dia_menos_pizza();
+        }
+        else if (strcmp(categorias, "apo") == 0){
+            promedio_pizzas_orden();
+        }
+        else if (strcmp(categorias, "apd") == 0){
+            promedio_pizzas_dia();
+        }
+        else if (strcmp(categorias, "ims") == 0){
+            ingrediente_mas_vendido();
+        }
+        else if (strcmp(categorias, "hp") == 0){
+            categoria_pizzas();
+        }
+    }
 
-    // Obtener la pizza más y menos vendida
-    char *mas_vendida = pizza_mas_vendida(iOrders, aOrders);
-    char *menos_vendida = pizza_menos_vendida(iOrders, aOrders);
-
-    // Mostrar los resultados
-    printf("Pizza más vendida: %s\n", mas_vendida);
-    printf("Pizza menos vendida: %s\n", menos_vendida);
-
-    // Liberar la memoria asignada
-    free_orders(aOrders, iOrders);  // Liberar las órdenes
+    // Liberar memorias asignadas
+    free_orders(aOrders, iOrders); // Liberar la memoria de las ordenes
     free(mas_vendida);   // Liberar la memoria de la pizza más vendida
     free(menos_vendida); // Liberar la memoria de la pizza menos vendida
-    
+    free(aux); // Liberar la memoria de la variable auxiliar en donde se almacena la categoría seleccionada
+
     return 0;
 }
